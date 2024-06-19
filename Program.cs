0097bdb335a3c5d12861fbd2450b912cd8c02f00
JJ <119088518+juliom1289@users.noreply.github.com>
2024-06-18 21:40:57 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿//Screen Sound
+//string mensagemBoasVindas = "Boas vindas ao Screen Sound";
+
+
+/*void ExibirMensagemDeBoasVindas() {
+    Console.WriteLine("***************************");
+    Console.WriteLine(mensagemBoasVindas);
+    Console.WriteLine("***************************");
+
+}*/
+
+//VERBATIN LITERAL
+//site: fsymsols.com
+void ExibirMensagemDeBoasVindas2() {
+    Console.WriteLine(@"
+╔═══╗──────────────╔═══╗──────────╔╗
+║╔═╗║──────────────║╔═╗║──────────║║
+║╚══╦══╦═╦══╦══╦═╗─║╚══╦══╦╗╔╦═╗╔═╝║
+╚══╗║╔═╣╔╣║═╣║═╣╔╗╗╚══╗║╔╗║║║║╔╗╣╔╗║
+║╚═╝║╚═╣║║║═╣║═╣║║║║╚═╝║╚╝║╚╝║║║║╚╝║
+╚═══╩══╩╝╚══╩══╩╝╚╝╚═══╩══╩══╩╝╚╩══╝");
+}
+
+void ExibirOpcoesDeMenu() {
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de avaliações de uma banda");
+    Console.WriteLine("Digite 5 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    int opcaoEscolhida = LeituraDeOpcoes();
+
+    while (opcaoEscolhida > 5 || opcaoEscolhida < 1) {
+        Console.WriteLine("Você escolheu uma opção inválida. Tente novamente");
+        opcaoEscolhida = LeituraDeOpcoes();
+    }
+
+    DeclaraOpcaoEscolhida(opcaoEscolhida);
+
+
+    static int LeituraDeOpcoes() {
+        string opcaoEscolhida = Console.ReadLine()!;
+        return int.Parse(opcaoEscolhida);
+    }
+
+    static void DeclaraOpcaoEscolhida(int opcao) {
+        Console.WriteLine("Você escolheu a opção " + opcao);
+    }
+
+
+} 
+
+ExibirMensagemDeBoasVindas2();
+ExibirOpcoesDeMenu();
